a4db0895716e65c560681a7fc3d26ab2d8c3d378
MarthinDaniel <76624094+MarthinDaniel@users.noreply.github.com>
2021-02-01 17:17:52 +0700

    Programming Exercise 1.1

--- Chapter_1/Exercise01_01.java
+++ Chapter_1/Exercise01_01.java
@@ -1,3 +1,5 @@
+// Write a program that displays Welcome to Java, Learning Java Now, and Programming is fun.
+
 public class Exercise_01_01 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
