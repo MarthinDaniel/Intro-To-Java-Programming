3e5fd557dbfc6c80391a2f7349be8475a0f62c2a
MarthinDaniel <76624094+MarthinDaniel@users.noreply.github.com>
2021-02-03 15:53:04 +0700

    Programming Exercise 2.7

---
+++ Chapter_02/Exercise02_07.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exercise02_07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of minutes: ");
+        int minutes = input.nextInt();
+
+        int years = minutes / 525600;
+        int days = (minutes % 525600) / 1440;
+
+        System.out.println(minutes + " minutes is approximately " + years + " years and " + days + " days.");
+        input.close(); 
+    }
+}
