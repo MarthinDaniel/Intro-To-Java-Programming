af50aacd86746fb89465877b4a2cffc2de38b4a2
MarthinDaniel <76624094+MarthinDaniel@users.noreply.github.com>
2021-02-03 11:39:17 +0700

    Programming Exercise 1.10

---
+++ Chapter_1/Exercise01_10.java
@@ -0,0 +1,13 @@
+public class Exercise01_10 {
+    public static void main(String[] args) {
+        double distance = 15;
+        double minutes = 50;
+        double temp = (50 * 60);
+        double second = temp + 30;
+        double hour = second / 3600;
+        double miles = 15 / 1.6;
+        double averageSpeed = miles / hour;
+
+        System.out.println(averageSpeed);
+    }
+}
