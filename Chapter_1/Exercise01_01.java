26dd73d5ca9c36cc4389a53a59c38a8ebee18c3d
MarthinDaniel <76624094+MarthinDaniel@users.noreply.github.com>
2021-02-11 14:14:55 +0700

    Delete Chapter_1 directory

--- Chapter_1/Exercise01_01.java
+++
@@ -1,9 +0,0 @@
-// Write a program that displays Welcome to Java, Learning Java Now, and Programming is fun.
-
-public class Exercise_01_01 {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java");
-		System.out.println("Welcome to Computer Science");
-		System.out.println("Programming is fun");
-	}
-}
--- Chapter_1/Exercise01_02.java
+++
@@ -1,9 +0,0 @@
-public class Exercise01_02 {
-    public static void main(String[] args) {
-        System.out.println("I Love Java");
-        System.out.println("I Love Java");
-        System.out.println("I Love Java");
-        System.out.println("I Love Java");
-        System.out.println("I Love Java");
-    }
-}
