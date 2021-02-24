b92a24fe132456fd016bf664256dd4c15111f7c1
MarthinDaniel <76624094+MarthinDaniel@users.noreply.github.com>
2021-02-24 11:54:53 +0700

    Programming Exercise 1.6

---
+++ Chapter01/Exercise06/Exercise01_06.java
@@ -0,0 +1,9 @@
+public class Exercise01_06{
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 11; i++){
+            count += i;
+        }
+        System.out.println(count);
+    }
+}
