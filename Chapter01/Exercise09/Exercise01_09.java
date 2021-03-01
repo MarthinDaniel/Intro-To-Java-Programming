6f45f57fdc7f48f83c7c7f897732ad1949505ecc
MarthinDaniel <76624094+MarthinDaniel@users.noreply.github.com>
2021-03-01 10:14:40 +0700

    Programming Exercise 1.9

---
+++ Chapter01/Exercise09/Exercise01_09.java
@@ -0,0 +1,12 @@
+public class Exercise01_09{
+    public static void main(String[] args) {
+        double width = 5.3;
+        double height = 8.6;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("The rectangle area of width " + width + " and height " + height + " is " + area);
+        System.out.println("The rectangle perimeter of width " + width + " and height " + height + " is " + perimeter);
+    }
+}
